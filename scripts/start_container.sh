2e4162800fe713e6036e0504f163335d2dc598c7
Laveena Kithani <71819423+Laveena1208@users.noreply.github.com>
2024-08-22 11:28:22 +0530

    added docker image path

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull simple-python-flask-app
+docker pull laveenak1208/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 simple-python-flask-app
+docker run -d -p 5000:5000 laveenak1208/simple-python-flask-app
